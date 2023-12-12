da0d3459afc9e0b392f150b7e6f5a22bdbd6277b
CodeErector <ham78625@gmail.com>
2023-12-12 22:36:30 +0500

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using linqQueries.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace linqQueries.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Student>? stu { get; set; }
+        public DbSet<Class>? Classes { get; set; }
+        public DbSet<Faculty>? Faculties { get; set; }
+        public DbSet<Enrolled>? Enrolled { get; set; }
+    }
+}
---
+++ Model/Enrolled.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace linqQueries.Model
+{
+    public class Enrolled
+    {
+        [Key]
+        public int eid { get; set; }
+
+        public virtual IList<Student>? students { get; set; }
+        public virtual IList<Class>? classes { get; set; }
+    }
+}
